a7afb6ec6e7040bc29dbcbde42ae0df9b4c57f32
mjbenefiel <michaeljbenefiel@gmail.com>
2018-10-22 17:18:41 -0500

    10222018 letter.js

---
+++ letter.js
@@ -0,0 +1,17 @@
+var Letter = function(character) {
+    this.character = character;
+    this.guessed = false;
+
+    this.toString = function() {
+        return this.guessed ? this.character : '_';
+    }
+
+    this.guessWord = function(newGuess) {
+        if (this.character.toLowerCase() === newGuess.toLowerCase()) {
+            this.guessed === true;
+        }
+    }
+}
+
+module.exports = Letter;
+console.log(Letter)
---
+++ word.js
@@ -0,0 +1,2 @@
+
+const Letter = require('./letter')
